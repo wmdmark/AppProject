c336a1181c6ab232c3df74acb9915ce0171602a1
Ethan Hansen <elihansen26@gmail.com>
2018-03-12 20:58:57 -0400

    initial files

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+class GetStarted extends React.Component {
+
+  static color = "red"
+
+  handlePress = (e)=> {
+    console.log("Button clicked!")
+  }
+
+  render() {
+    return <View>
+      <Button style={{color: this.color}} title={this.props.label} onPress={this.handlePress} />
+    </View>
+  }
+
+}
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello world!</Text>
+        <GetStarted label="Getting started" />
+        <GetStarted label="Cancel" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
